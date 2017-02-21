01db70915bb895ab3276cc8181d473d879536459
karolina.gkallajian <karolina.gkallajian@CAS511780W0726.senacsp.edu.br>
2017-02-21 20:48:51 -0300

    Boa Noite Mundo nas estrelas

--- testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -10,9 +10,16 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Boa noite, mundo");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("zzz");
+
+        for (int i = 0; i <= 10; i++) {
+            if (i == 0 || i == 10) {
+                System.out.println("**********************");
+            } else if (i == 5) {
+                System.out.println("*  Boa noite, mundo  *");
+            } else {
+                System.out.println("*                    *");
+            }
+
         }
 
     }
