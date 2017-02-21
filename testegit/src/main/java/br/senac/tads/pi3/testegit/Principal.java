d6365aeb443f874b890351c35a67cb83345b16e6
karolina.gkallajian <karolina.gkallajian@CAS511780W0726.senacsp.edu.br>
2017-02-21 20:35:29 -0300

    Versão inicial

---
+++ testegit/src/main/java/br/senac/tads/pi3/testegit/Principal.java
@@ -0,0 +1,17 @@
+
+package br.senac.tads.pi3.testegit;
+
+/**
+ *
+ * @author karolina.gkallajian
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Boa noite, mundo zzzzz");
+    }
+    
+}
